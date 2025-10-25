a3dd4254a16ff3641a0c60f3b05b383634a86dd6
Shreeraam G <shreeram0312@gmail.com>
2025-10-25 14:06:15 +0000

    AI Chat bot API with gemini

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { GoogleGenAI } from '@google/genai'
 
 dotenv.config()
 
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
+
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
@@ -13,6 +18,44 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' })
+})
+
+type ChatHistory = {
+  role: 'user' | 'model'
+  parts: { text: string }[]
+}
+
+const conversations = new Map<string, ChatHistory[]>()
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+  const { prompt, conversationId } = req.body
+
+  const conversationHistory: ChatHistory[] =
+    conversations.get(conversationId) ?? []
+
+  const chat = ai.chats.create({
+    model: 'gemini-2.5-flash-lite',
+    history: conversationHistory,
+    config: {
+      temperature: 0.2,
+      maxOutputTokens: 80
+    }
+  })
+
+  conversationHistory?.push({ role: 'user', parts: [{ text: prompt }] })
+
+  const response = await chat.sendMessage({
+    message: prompt
+  })
+
+  conversationHistory?.push({
+    role: 'model',
+    parts: [{ text: response.text || 'No Response from model' }]
+  })
+
+  conversations.set(conversationId, conversationHistory)
+
+  res.json({ message: response.text })
 })
 
 app.listen(port, () => {
